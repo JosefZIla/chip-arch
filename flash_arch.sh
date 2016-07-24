f896663bb9e61e507523866e8677ef0303d72aea
Josef Zila <josefzila@gmail.com>
2016-07-24 19:40:27 +0000

    First Arch on C.H.I.P.

---
+++ flash_arch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Loading SPL to the device"
+sudo sunxi-fel spl sunxi-spl.bin
+sleep 1
+
+echo "Uploadnig padded SPL to device memory"
+sudo sunxi-fel write 0x43000000 sunxi-spl-with-ecc.bin
+
+echo "Uploading padded Uboot to device memory"
+sudo sunxi-fel write 0x4a000000 u-boot-dtb.bin
+
+echo "Uploading Uboot flashing script image to device memory"
+sudo sunxi-fel write 0x43100000 uboot-script.img
+
+echo "Running Uboot with flashing script on device and  waiting for fastboot"
+sudo sunxi-fel exe 0x4a000000
+
+timeout=30
+while let "timeout > 0"; do
+  test -z $(fastboot -i 0x1f3a devices) && break
+  let timeout--
+done
+let timeout==0 &&{
+  echo "cannot find device in fastboot mode"
+  exit -1
+}
+echo "Flashing UBI image using fastboot"
+sudo fastboot -i 0x1f3a -u flash UBI ubi.img.sparse
+
+echo "Falshing done, hopefully now the device will boot correctly to Arch"
+sudo fastboot -i 0x1f3a continue
